94efcef98a87a2a09a3e9f4dfd59a2e6d3b3117d
Rasmus Israelsson <IsraelssonRasmusRaz@gmail.com>
2016-05-16 16:05:00 +0200

    removing random jumping

--- apps/system/about/about.js
+++ apps/system/about/about.js
@@ -3,5 +3,5 @@ importScripts("/web32.js");
 function main()
 {
     let window = new web32.Window();
-    window.setPosition(Math.random() * 500, Math.random() * 500);
+    window.setPosition(100, 100);
 }
