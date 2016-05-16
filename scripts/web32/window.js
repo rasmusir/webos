c6bbe2c246d6576fe5d7da9511cdc88decbb3c52
Rasmus Israelsson <IsraelssonRasmusRaz@gmail.com>
2016-05-16 18:24:01 +0200

    adding createElement() and addElement()

--- scripts/web32/window.js
+++ scripts/web32/window.js
@@ -6,23 +6,18 @@ web32.Window = class Window extends web32.Module
     {
         super();
         this.createHost();
+        this.elements = new Map();
     }
 
-    onHost(id)
+    onHost()
     {
         let template = document.querySelector("#window");
         let win = document.importNode(template.content, true).firstElementChild;
         document.body.appendChild(win);
         this._window = win;
-
+        this.element = win.querySelector(".content");
 
         this.h_setTitle(this.id);
-    }
-
-    h_setPosition(x, y)
-    {
-        this._window.style.left = x + "px";
-        this._window.style.top = y + "px";
     }
 
     h_setTitle(title)
@@ -32,10 +27,25 @@ web32.Window = class Window extends web32.Module
         t.appendChild(document.createTextNode(title));
     }
 
-    setPosition(x, y)
+    addElement(element)
     {
-        this.host("setPosition", x, y);
+        this.elements.set(element.id, element);
+    }
+
+    setPosition(x, y) { this.host("setPosition", x, y); }
+    h_setPosition(x, y)
+    {
+        this._window.style.left = x + "px";
+        this._window.style.top = y + "px";
+    }
+
+    createElement(type, properties)
+    {
+        let element = new web32.Element(this, type, properties);
+        this.addElement(element);
+        return element;
     }
 };
+
 
 web32.Module.register(web32.Window);
