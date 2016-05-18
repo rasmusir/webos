a54d6bb6f575cfddf510d616a7c68fba327ae7f3
Rasmus Israelsson <IsraelssonRasmusRaz@gmail.com>
2016-05-19 01:27:19 +0200

    Web32 -> WebOS

--- apps/system/about/about.js
+++ apps/system/about/about.js
@@ -7,14 +7,18 @@ function *main()
     let window = new web32.Window();
     window.setPosition(100, 100);
 
+    window.on("close", close);
+
     let container = window.createElement("div");
     container.set("style", "text-align", "center");
 
     let text = container.createElement("div");
-    text.set("innerHTML", `Web32 is created by Rasmus Israelsson<br>You can find the github <a target="_blank" href="https://github.com/rasmusir/webos">Here</a><br><br>`);
+    text.set("innerHTML", `WebOS is created by Rasmus Israelsson<br>You can find the github <a target="_blank" href="https://github.com/rasmusir/webos">Here</a><br><br>`);
 
     let button = container.createElement("button");
     button.set("innerHTML", "Close");
 
     button.on("click", close);
+
+    window.show();
 }
