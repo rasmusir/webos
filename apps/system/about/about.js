d93df78c86ee8d5137f078ddb305fa7a2b32ebf2
Rasmus Israelsson <IsraelssonRasmusRaz@gmail.com>
2016-05-18 23:17:58 +0200

    Using sync() to create an about "app"

--- apps/system/about/about.js
+++ apps/system/about/about.js
@@ -1,7 +1,20 @@
+"use strict";
 importScripts("/web32.js");
 
-function main()
+function *main()
 {
+    let sync = yield;
     let window = new web32.Window();
     window.setPosition(100, 100);
+
+    let container = window.createElement("div");
+    container.set("style", "text-align", "center");
+
+    let text = container.createElement("div");
+    text.set("innerHTML", `Web32 is created by Rasmus Israelsson<br>You can find the github <a target="_blank" href="https://github.com/rasmusir/webos">Here</a><br><br>`);
+
+    let button = container.createElement("button");
+    button.set("innerHTML", "Close");
+
+    button.on("click", close);
 }
