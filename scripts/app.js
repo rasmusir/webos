6634d5e2d726f2fc7955fba7748dc806e6551460
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2016-05-19 16:30:30 +0000

    Installing system.appdrawer

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ function main()
 {
 
     let s = new System();
-    s.installAll("apps/system/about", "apps/system/taskbar").then(apps => {
+    s.installAll("apps/system/about", "apps/system/taskbar", "apps/system/appdrawer").then(apps => {
         s.run("system.taskbar");
 
     });
