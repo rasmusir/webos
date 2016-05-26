d96b19ecd0c66923f397010d3f9c474a85c006c6
Rasmus Israelsson <IsraelssonRasmusRaz@gmail.com>
2016-05-26 03:51:40 +0200

    Launching links in system.explorer

--- apps/system/about/about.js
+++ apps/system/about/about.js
@@ -12,7 +12,19 @@ function *main()
     container.set("style", "text-align", "center");
 
     let text = container.createElement("div");
-    text.set("innerHTML", `WebOS is created by Rasmus Israelsson<br>You can find the github <a target="_blank" href="https://github.com/rasmusir/webos">Here</a><br><br>`);
+    text.set("innerHTML", `WebOS is created by Rasmus Israelsson<br>You can find the github <a target="_blank" href="https://github.com/rasmusir/webos">Here</a><br>He's a student at <a target="_blank" href="https://lnu.se/">Linneaus University</a><br> <br>`);
+    text.on("click", (data) => {
+        if (data)
+        {
+            web32.Interface.launch("system.explorer", data.url);
+        }
+    }, (event) => {
+        if (event.target.tagName === "A")
+        {
+            event.preventDefault();
+            return {url: event.target.href};
+        }
+    });
 
     let button = container.createElement("button");
     button.set("innerHTML", "Close");
